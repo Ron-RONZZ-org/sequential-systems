b8b365891e1ca8947e862b53d27550327a503307
Rong Zhou <ron@ronzz.org>
2026-04-02 11:22:52 +0200

    Digital O

--- NI-DAQmx/P0-O.py
+++ NI-DAQmx/P0-O.py
@@ -3,12 +3,12 @@ import nidaqmx
 from nidaqmx.constants import LineGrouping
 
 DEVICE = "Dev1"
-LINES = [f"{DEVICE}/port0/line{i}" for i in range(8)]
+# LINES = [f"{DEVICE}/port0/line{i}" for i in range(8)]
 DELAY_S = 0.5
 
 with nidaqmx.Task() as task:
     task.do_channels.add_do_chan(
-        LINES,
+        f"{DEVICE}/port0/line0:7",
         line_grouping=LineGrouping.CHAN_PER_LINE
     )
     pattern = [False] * 8
